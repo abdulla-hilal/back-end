f08485f861b1197c68fc0c1221d7d48e8b190794
abdulla-hilal <163716797+abdulla-hilal@users.noreply.github.com>
2024-03-17 17:07:44 +0000

    12 - Starting the Back-end

--- server.js
+++ server.js
@@ -11,5 +11,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
